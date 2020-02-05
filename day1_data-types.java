0d0d0aa5c77e76c399b0f866521070072c9f2bce
mell1022 <mell.mmm.1022@gmail.com>
2020-02-05 14:42:56 +0900

    データタイプ

---
+++ day1_data-types.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int inputInt;
+        double inputDouble;
+        String inputString;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        // Scanner scan = new Scanner(System.in); 
+        inputInt = scan.nextInt();
+        inputDouble = scan.nextDouble();
+        String tmp = scan.nextLine(); // unnecessary :(
+        inputString = scan.nextLine();
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + inputInt);
+
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d + inputDouble);
+
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+            System.out.println(s + inputString);
+
+        scan.close();
+     }
+}
